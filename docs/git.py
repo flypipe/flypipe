9b9192c702c1e6d960bcd5e4a83eb672e7265b63
jose.muller <jose.muller@prospa.com>
2022-09-08 13:31:46 +1000

    changin order get contributors

--- docs/git.py
+++ docs/git.py
@@ -16,3 +16,5 @@ with open(os.path.join(os.getcwd(), "source/readmes/contributors.md"), "w") as f
     for contributor in contributors:
         f.write(f"""\n- [@{contributor['author']['login']}]({contributor['author']['html_url']}) {contributor['total']} commits""")
 
+
+f.close()
