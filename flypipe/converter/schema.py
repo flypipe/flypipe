6aba3adde2d41bf0cd651d3d50d50d07a1d1db99
jose.muller <jose.muller@prospa.com>
2022-09-28 09:33:02 +1000

    adding documentation to schema converter

--- flypipe/converter/schema.py
+++ flypipe/converter/schema.py
@@ -2,9 +2,26 @@ from flypipe.schema.schema import Schema
 
 
 class SchemaConverter:
+    """Casts a dataframe using a given schema"""
 
     @staticmethod
     def cast(df, schema: Schema):
+        """Casts a dataframe using a given schema
+
+        Parameters
+        ----------
+
+        df: pandas, pandas_on_spark or spark dataframe
+            dataframe to be casted
+        schema: Schema
+            the schema definition in which the columns of the dataframe will be casted to the defined data types
+
+        Returns
+        -------
+        df: pandas, pandas_on_spark or spark dataframe
+            dataframe casted to the given schema
+        """
+
         for column in schema.columns:
             df = column.type.cast(df, column.name)
         return df
