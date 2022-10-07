be9734eee506938c1307ea0c2e7e43c1f53ff242
jose.muller <jose.muller@prospa.com>
2022-10-07 11:04:40 +1100

    fixing datasource table name

--- flypipe/node.py
+++ flypipe/node.py
@@ -59,7 +59,7 @@ class Transformation:
     @property
     def __name__(self):
         """Return the name of the wrapped transformation rather than the name of the decorator object"""
-        return self.function.__name__
+        return self.function.__name__.replace(".","_")
 
     @property
     def __doc__(self):
