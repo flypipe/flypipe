b07152318494816701ac65d65785cc4aca4b6b6f
Chris Marwick <chris.marwick@prospa.com>
2023-01-11 12:18:28 +1100

    chore: more

---
+++ scripts/set_version.py
@@ -0,0 +1,11 @@
+import toml
+from sys import argv
+
+if len(argv) != 2:
+    raise TypeError('Expected set_version to be invoked with a single argument <version>')
+
+version = argv[1]
+project_data = toml.load('pyproject.toml')
+project_data['project']['version'] = version
+with open('pyproject.toml', 'w') as f:
+    toml.dump(project_data, f)
