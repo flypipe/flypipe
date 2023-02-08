024269799e44cd7f45f07dba85dfd8d0507eb10a
Chris Marwick <chris.marwick@prospa.com>
2023-02-08 12:13:13 +1100

    feat: add utility script to sync the frontend js bundle automatically

---
+++ frontend/scripts/copy-to-flypipe.js
@@ -0,0 +1,12 @@
+// Once we build the js artifact containing all the frontend code we need to 
+// copy this into the Flypipe python code so that it can use the artifact. 
+
+var fs = require('fs');
+
+fs.copyFile('dist/bundle.js', '../flypipe/js/bundle.js', (err) => {
+    if (err) {
+        console.log('ERROR copying bundle.js to Flypipe Python code');
+    } else {
+        console.log('Successfully copied bundle.js to Flypipe Python code');
+    }
+});
