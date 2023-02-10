4f0bec200b6841f2c529c9efa096e6fc3df2c22c
Chris Marwick <chris.marwick@prospa.com>
2023-02-10 12:14:29 +1100

    chore: remove old code

--- frontend/src/catalog/sidebar/item.jsx
+++
@@ -1,9 +0,0 @@
-import React, {useState} from 'react';
-
-const Item = ({title, isSelected, onClick, ...props}) => {
-    return <li className="nav-item">
-        <a className="nav-link active" onClick={() => {onClick()}} {...props}>{title}{isSelected ? '!' : ''}</a>
-    </li>
-};
-
-export default Item;
