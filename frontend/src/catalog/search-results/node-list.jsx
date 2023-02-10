8f795cdb7fbd43f2e6fcfc03102875d9dbde94f1
Chris Marwick <chris.marwick@prospa.com>
2023-02-11 01:38:55 +1100

    feat: add toast notifications for adding to node builder, temporarily disabled until node builder is done

--- frontend/src/catalog/search-results/node-list.jsx
+++ frontend/src/catalog/search-results/node-list.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useMemo, useCallback, useEffect, useRef} from 'react';
 import Node from './node';
 import Pagination from './pagination';
+import Notifications from './notifications';
 
 // Maximum number of search entries per page
 const SEARCH_PAGE_SIZE = 8;
@@ -11,6 +12,10 @@ const SEARCH_PAGE_GROUP_SIZE = 5;
 const NodeList = ({nodes, selectedNode, handleSelectNode}) => {
     const [currentNodes, setCurrentNodes] = useState([]);
     const [graphBuilderNodes, setGraphBuilderNodes] = useState(new Set());
+    const [notification, setNotification] = useState({
+        msgId: 0,
+        message: ""
+    });
     // Use need to use a ref to access the updated graphBuilderNodes 
     // (https://stackoverflow.com/questions/55265255/react-usestate-hook-event-handler-using-initial-state)
     // Still unsure why exactly the state doesn't update in callbacks here in particular though as it 
@@ -34,8 +39,12 @@ const NodeList = ({nodes, selectedNode, handleSelectNode}) => {
     }, [nodesByPage]);
 
     const handleClickGraphBuilder = (nodeKey) => {
-        const nodes = graphBuilderNodesRef.current;
-        if (nodes.has(nodeKey)) {
+        const graphBuilderNodes = graphBuilderNodesRef.current;
+        const {name} = nodes.find((node) => node.nodeKey === nodeKey);
+        const notificationMessage = graphBuilderNodes.has(nodeKey) ? 
+            `Removed ${name} from the Graph Builder` : 
+            `Added ${name} to the Graph Builder`;
+        if (graphBuilderNodes.has(nodeKey)) {
             setGraphBuilderNodes((prevGraphBuilderNodes) => {
                 const newSet = new Set([...prevGraphBuilderNodes].filter(x => x !== nodeKey));
                 graphBuilderNodesRef.current = newSet;
@@ -48,6 +57,10 @@ const NodeList = ({nodes, selectedNode, handleSelectNode}) => {
                 return newSet;
             });
         }
+        setNotification(({msgId}) => ({
+            msgId: msgId + 1,
+            message: notificationMessage
+        }));
     };
     
     return <div className="list-group list-group-flush">
@@ -65,6 +78,7 @@ const NodeList = ({nodes, selectedNode, handleSelectNode}) => {
             />
         )}
         <Pagination maxPage={maxPage} pageGroupSize={SEARCH_PAGE_GROUP_SIZE} handleClickPage={handleChangePage}/>
+        {/* <Notifications newMessage={notification}/> */}
     </div>
 };
 
--- frontend/src/catalog/search-results/node.jsx
+++ frontend/src/catalog/search-results/node.jsx
@@ -6,10 +6,11 @@ const Node = ({nodeKey, name, importCmd, description, isInGraphBuilder=false, se
     const graphBuilderButton = useMemo(() => {
         const btnText = isInGraphBuilder ? "Remove" : "Add";
         const tooltipText = isInGraphBuilder ? "Remove node from the Graph Builder" : "Add node to the Graph Builder";
-        const buttonClass = isInGraphBuilder ? "btn-danger" : "btn-success";
+        const buttonClass = isInGraphBuilder ? "btn-danger" : "btn-light";
         return <button 
             className={classNames(
                 "btn",
+                "btn-sm",
                 buttonClass,
             )}
             data-elem-name="graph-builder-button"
@@ -35,7 +36,7 @@ const Node = ({nodeKey, name, importCmd, description, isInGraphBuilder=false, se
     }}>
         <div className="d-flex justify-content-between">
             <label className="form-check-label" htmlFor={`nodeCheckbox-${name}`}><span className="fw-bold">{name}</span></label>
-            {graphBuilderButton}
+            {/* {graphBuilderButton} */}
         </div>
         <p>{description}</p>
     </a>
