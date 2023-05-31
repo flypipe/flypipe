9f78cee6d7b855af54e440b007a75adbc5c4f21a
Jose Muller <jose.muller@deferit.com>
2023-05-31 21:19:06 +0800

    adding cases of showing/hidding edges between groups and nodes

--- frontend/src/group.js
+++ frontend/src/group.js
@@ -56,6 +56,9 @@ export class Group {
         const group = nodes.find(({ id }) => id === this.groupId);
         const otherNodes = nodes.filter(({ id }) => id !== this.groupId);
         const groupNodes = nodes.filter(({ id }) => group.data.nodes.has(id));
+
+//        console.log("----- GROUP", group.data.label, isMinimise, "-----");
+
         // All nodes within the group should be hidden if we are minimising and shown if maximising
         groupNodes.forEach((groupNode) => {
             groupNode.hidden = isMinimise;
@@ -66,14 +69,9 @@ export class Group {
             ({ source, target }) =>
                 group.data.nodes.has(source) && group.data.nodes.has(target)
         );
-        // All edges to and from nodes within the group should be hidden if we are minimising and shown if maximising
-        edges
-            .filter(
-                ({ source, target }) =>
-                    group.data.nodes.has(source) || group.data.nodes.has(target)
-            )
-            .forEach((edge) => (edge.hidden = isMinimise));
-        // All edges to and from the group node must be set to visible if minimising and hidden if maximising
+        // Show or hide internal edges in group
+        groupEdges.forEach((edge) => (edge.hidden = isMinimise))
+
         // We also need to update the source/target name if to match the new group node name
         edges
             .filter(
@@ -81,7 +79,176 @@ export class Group {
                     source === group.id || target === group.id
             )
             .forEach((edge) => {
-                edge.hidden = !isMinimise;
+                //console.log("");
+                const source_node = nodes.find(({ id }) => id === edge.source);
+                const source_node_is_group = source_node.type === "flypipe-group";
+                const source_group_node = source_node_is_group? source_node : (source_node.data.group ? getGroup(this.graph, source_node.data.group) : null);
+
+                const target_node = nodes.find(({ id }) => id === edge.target);
+                const target_node_is_group = target_node.type === "flypipe-group";
+                const target_group_node = target_node_is_group? target_node : (target_node.data.group ? getGroup(this.graph, target_node.data.group) : null);
+
+
+//                console.log("Edge:", edge.source, "->", edge.target);
+                edge.hidden = true;
+                if (source_node_is_group && target_node_is_group){
+//                    console.log("case 0");
+                    if(source_group_node.data.isMinimised && target_group_node.data.isMinimised) {
+//                        console.log("case 0.1", edge);
+                        edge.hidden = false;
+                    }
+                    else{
+//                        console.log("case 0.2", edge);
+                        edge.hidden = true;
+                    }
+                }
+                else if (!source_node_is_group){
+                    if (target_group_node.data.isMinimised){
+//                        console.log("case 1: source is node, target is group");
+                        // show edge from non-group node to group node
+                        if (source_group_node == null){
+//                            console.log("case 1.1", edge);
+                            edge.hidden = false;
+                        }
+                        else if (source_group_node.data.isMinimised){
+
+                            if (target_group_node.data.isMinimised){
+//                                console.log("case 1.2.1", edge);
+                                edge.hidden = true;
+                            }
+                            else{
+//                                console.log("case 1.2.2", edge);
+                                edge.hidden = false;
+                            }
+                        }
+                        else if (target_group_node == null){
+//                            console.log("case 1.3", edge);
+                            edge.hidden = false;
+                        }
+                        else if (target_group_node.data.isMinimised){
+//                            console.log("case 1.4", edge);
+                            edge.hidden = false;
+                        }
+
+
+                        // hide all edges from source internal nodes to target_group
+                        const internalTargetNodes = nodes.filter((node) => node.data.group === target_group_node.data.label);
+                        internalTargetNodes.forEach(( internalTargetNode ) => {
+                            internalTargetNode.data.predecessors.forEach(( predecessorNodeId ) => {
+                                if (predecessorNodeId == source_node.id){
+                                    edges
+                                    .filter(
+                                        ({ source, target }) => {
+                                            return source == source_node.id && target == internalTargetNode.id
+                                        }
+                                    )
+                                    .forEach((edge) => {
+//                                        console.log("case 1.5:", predecessorNodeId, "--->", internalTargetNode.id);
+                                        edge.hidden = true;
+                                    });
+
+                                }
+                            })
+                        });
+
+                    }
+                    else{
+//                        console.log("case 2");
+                        // hide edge from non-group node to group node
+                        edge.hidden = true;
+
+                        // show all edges from non-group node to internal nodes of group node
+                        const internalTargetNodes = nodes.filter((node) => node.data.group === target_group_node.data.label);
+                        internalTargetNodes.forEach(( internalTargetNode ) => {
+                            internalTargetNode.data.predecessors.forEach(( predecessorNodeId ) => {
+                                if (predecessorNodeId == source_node.id){
+                                    edges
+                                    .filter(
+                                        ({ source, target }) => {
+                                            return source == source_node.id && target == internalTargetNode.id
+                                        }
+                                    )
+                                    .forEach((edge) => {
+//                                        console.log("case 2.1:", predecessorNodeId, "--->", internalTargetNode.id);
+                                        edge.hidden = false;
+                                    });
+
+                                }
+                            })
+                        });
+                    }
+                }
+                else{
+//                    console.log("case 3: source is group, target is node");
+                    const target_node_is_visible = !target_node.hidden;
+
+                    if (target_node_is_visible) {
+//                        console.log("case 3.1", edge);
+                        edge.hidden=false;
+                    }
+                    else{
+//                        console.log("case 3.2", edge);
+                        edge.hidden=true;
+                    }
+
+                    if (source_group_node.data.isMinimised){
+//                        console.log("case 3.3", edge);
+
+                        //hide all edges from source internal nodes
+                        const internalSourceNodes = nodes.filter((node) => node.data.group === source_group_node.data.label);
+                        internalSourceNodes.forEach(( internalSourceNode ) => {
+
+                            if (target_node.data.predecessors.includes(internalSourceNode.id)){
+                                edges
+                                .filter(
+                                    ({ source, target }) => {
+                                        return source == internalSourceNode.id && target == target_node.id
+                                    }
+                                )
+                                .forEach((edge) => {
+//                                    console.log("case 3.3.1", edge);
+                                    edge.hidden = true;
+                                });
+                            }
+
+                        });
+                    }
+                    else{
+//                        console.log("case 4.1");
+                        if (target_group_node != null){
+//                            console.log("case 4.2", edge);
+                            edge.hidden = true;
+                        }
+                        else {
+//                            console.log("case 4.3");
+                            edge.hidden = true;
+
+                            //show all edges from internal nodes of source group to target node
+                            const internalSourceNodes = nodes.filter((node) => node.data.group === source_group_node.data.label);
+                            internalSourceNodes.forEach(( internalSourceNode ) => {
+                                if (target_node.data.predecessors.includes(internalSourceNode.id)){
+                                    edges
+                                    .filter(
+                                        ({ source, target }) => {
+                                            return source == internalSourceNode.id && target == target_node.id
+                                        }
+                                    )
+                                    .forEach((edge) => {
+//                                        console.log("case 4.3.1");
+                                        edge.hidden = false;
+                                    });
+                                }
+
+                            });
+
+
+                        }
+                    }
+
+
+
+                }
+
                 if (edge.source === group.id) {
                     edge.source = newGroupId;
                 } else {
