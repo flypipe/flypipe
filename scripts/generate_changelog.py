1afe3e2a90346513cce1ba5faf39a93ec5fbb10a
josemuller <joseheliomuller@gmail.com>
2025-05-29 14:09:07 -0300

    fix changelogs and add to docs

--- scripts/generate_changelog.py
+++ scripts/generate_changelog.py
@@ -1,9 +1,17 @@
 import base64
 import os
+import pathlib
+
 import requests
 import re
 import subprocess
 
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from scripts.utils import prepend_lines_to_file
+from scripts.utils import get_release_branches, get_commit_list
+from scripts.calculate_version import get_commit_message, calculate_version
 
 RE_GITHUB_ISSUE = re.compile(r'#\d+')
 GITHUB_URL = 'https://api.github.com/repos/flypipe/flypipe'
@@ -11,54 +19,49 @@ GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
 github_connection = requests.session()
 
 
-# Get commit list between HEAD and the most recent version number
-all_branches = (
-    subprocess.check_output('git for-each-ref --format="%(refname:short)"', shell=True)
-    .decode("utf-8")
-    .split()
-)
-release_branches = [
-    branch for branch in all_branches if branch.startswith("origin/release/")
-]
-if not release_branches:
-    # Unable to find a previous release
-    latest_version = [0, 0, 0]
-    commit_list = (
-        subprocess.check_output(f"git rev-list HEAD --no-merges", shell=True)
-        .decode("utf-8")
-        .split()
-    )
-else:
-    latest_version_branch_name = max(release_branches)
-    # release branch name will be origin/release/x.y.z, we remove the first 15 characters to just get the version
-    latest_version = [int(num) for num in latest_version_branch_name[15:].split(".")]
-    commit_list = (
-        subprocess.check_output(
-            f"git rev-list {latest_version_branch_name}..HEAD --no-merges", shell=True
-        )
-        .decode("utf-8")
-        .split()
-    )
 
-issues = {}
-for commit_id in commit_list:
-    commit_message = subprocess.check_output(
-        f"git show {commit_id} -s --format=%B", shell=True
-    ).decode("utf-8")
-    commit_message_summary = commit_message.split("\n", maxsplit=1)[0]
-    re_match = re.search(RE_GITHUB_ISSUE, commit_message_summary)
-    if re_match:
-        print(f'Found github issue {re_match.group(0)} in commit msg summary {commit_message_summary}')
-        issue_id = re_match.group(0)[1:]
-        issue = github_connection.get(f'{GITHUB_URL}/issues/{issue_id}', headers={'Authorization': f'Bearer {GITHUB_TOKEN}'}).json()
-        if 'title' not in issue:
-            print(f'Unable to find title for issue {issue_id}')
-            continue
-        issues[issue_id] = issue['title']
+def generate_changelog(to_branch: str=None):
+    to_branch = to_branch or "HEAD"
+    # Get commit list between HEAD and the most recent version number
+    release_branches = get_release_branches()
+    if not release_branches:
+        # Unable to find a previous release
+        commit_list = get_commit_list(to_branch=to_branch)
+    else:
+        latest_version_branch_name = max(release_branches)
+        commit_list = get_commit_list(from_branch=latest_version_branch_name, to_branch=to_branch)
+
+    issues = {}
+    for commit_id in commit_list:
+        commit_message = get_commit_message(commit_id)
+        commit_message_summary = commit_message.split("\n", maxsplit=1)[0]
+        re_match = re.search(RE_GITHUB_ISSUE, commit_message_summary)
+        if re_match:
+            print(f'Found github issue {re_match.group(0)} in commit msg summary {commit_message_summary}')
+            issue_id = re_match.group(0)[1:]
+            url = f'{GITHUB_URL}/issues/{issue_id}'
+            issue = github_connection.get(url, headers={'Authorization': f'Bearer {GITHUB_TOKEN}'}).json()
+            if 'title' not in issue:
+                print(f'Unable to find title for issue {issue_id}')
+                continue
+            issues[issue_id] = f'<a href="https://github.com/flypipe/flypipe/issues/{issue_id}" target="_blank" rel="noopener noreferrer">{issue_id} {issue["title"]}</a>'
 
 
-issue_ids = sorted(list(issues.keys()))
-with open('changelog.txt', 'w', encoding='utf-8') as f:
-    f.writelines(['Changelog\n', '*******\n'])
+    return issues
+
+
+def save_changelog(issues, version):
+    issue_ids = sorted(list(issues.keys()))
     lines = [f'- {issues[issue_id]}\n' for issue_id in issue_ids]
-    f.writelines(lines)
+
+    path = os.path.join(pathlib.Path(__file__).parent.parent.resolve(), "changelog.md")
+    prepend_lines_to_file(path, lines, version)
+
+if __name__ == '__main__':
+    import sys
+    commands = sys.argv[1:]
+    to_branch = "HEAD" if not commands else commands[0]
+    issues = generate_changelog(to_branch,)
+
+    version = calculate_version(to_branch=to_branch)
+    save_changelog(issues, version)
