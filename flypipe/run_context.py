476d5a0d122c88fd8da5d62820df3b197deb8967
josemuller <joseheliomuller@gmail.com>
2025-05-30 03:28:49 -0300

    except import pyspark.pandas in case sparkleframe is activated

--- flypipe/run_context.py
+++ flypipe/run_context.py
@@ -3,7 +3,12 @@ from dataclasses import dataclass, field
 from typing import Mapping, Union
 
 from pandas import DataFrame as PandasDataFrame
-from pyspark.pandas.frame import DataFrame as PandasApiDataFrame
+
+try:  # when activating sparkleframe, it does not implement pyspark.pandas
+    from pyspark.pandas.frame import DataFrame as PandasApiDataFrame
+except ModuleNotFoundError:
+    from pandas import DataFrame as PandasApiDataFrame
+
 from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame as PySparkDataFrame
 
