82a4dee74f528b1e437a347b408e57ca7c3add16
jose.muller <jose.muller@withclutch.com>
2025-06-18 02:35:14 -0300

    #190-change-documentation-from-sphinx-to-mkdocs add node function

--- flypipe/node_function.py
+++ flypipe/node_function.py
@@ -108,35 +108,27 @@ def node_function(*args, **kwargs):
     """
     Decorator factory that returns the given function wrapped inside a NodeFunction class
 
-    Parameters
-    ----------
+    Parameters:
+        requested_columns (bool,optional): List of requested columns that successors nodes are demanding from the node
+        function, if True will retrieve `requested_columns` as named argument. Defaults to `False`.
+    node_dependencies (List[Node or NodeFunction],optional): List of external nodes that the node function is
+        dependent on. Any node retrieved by the node function (called internal node) can only be dependent on any
+        internal node or any node inside `node_dependencies`. True, returns spark context as argument to the function.
+        Defaults to `False`.
+    output (Schema,optional): Defines the output schema of the node. Defaults to `None`.
 
-    requested_columns : bool, optional (default `False`)
-        List of requested columns that successors nodes are demanding from the node function.
-        if True will retrieve `requested_columns` as named argument
-    node_dependencies : List[Node or NodeFunction], optional
-        List of external nodes that the node function is dependent on.
-        Any node retrieved by the node function (called internal node) can only be dependent on any internal node or
-        any node inside `node_dependencies`.
-        True, returns spark context as argument to the function (default is False)
-    output : Schema, optional
-        Defines the output schema of the node (default is None)
+    Returns:
+        List[Node]
+            a list of nodes created internally
 
-    Returns
-    -------
-    List[Node]
-        a list of nodes created internally
+    Raises:
+        ValueError
+            If any internal node is of type NodeFunction; if any internal node has a dependency that is not to another
+            internal node and not declared in node_dependencies
 
-    Raises
-    ------
-    ValueError
-        If any internal node is of type NodeFunction; if any internal node has a dependency that is not to another
-        internal node and not declared in node_dependencies
+    # Examples:
 
-
-    .. highlight:: python
-    .. code-block:: python
-
+    ```python
         # Syntax
         @node_function(
             requested_columns=True,
@@ -154,8 +146,9 @@ def node_function(*args, **kwargs):
             )
             def internal_node_1(df):
                 return df
+    ```
 
-
+    ```python
             @node(
                 type="pandas",
                 dependencies=[
@@ -166,6 +159,7 @@ def node_function(*args, **kwargs):
                 return df
 
             return internal_node_1, internal_node_2 # <-- ALL INTERNAL NODES CREATED MUST BE RETURNED
+    ```
 
     """
 
