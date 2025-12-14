787ab35289b41f93e79527f18e6c6194e1c0ddfc
josemuller <joseheliomuller@gmail.com>
2025-12-14 20:53:11 -0300

    #209-feature-request-implement-cdc-cache-in-flypipe-single-cache Add populate_cached_successors method to RunContext
    - Added populate_cached_successors() method to handle nested node_results structure
    - Method iterates over provided_inputs nodes and finds their cached successors
    - Loads cache for successors and stores in node_results[input_node][successor_node]
    - Updated node_results type annotation from Mapping to Autodict
    - Removed unused Mapping import

--- flypipe/run_context.py
+++ flypipe/run_context.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Mapping, Union, TYPE_CHECKING
+from typing import Union, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from flypipe.node import Node
@@ -26,6 +26,10 @@ from flypipe.dependency.preprocess_mode import PreprocessMode
 from flypipe.config import get_config
 from flypipe.node_result import NodeResult
 
+class Autodict(dict):
+    def __missing__(self, key):
+        value = self[key] = type(self)()
+        return value
 
 @dataclass
 class RunContext:
@@ -42,7 +46,7 @@ class RunContext:
     cache_modes: dict = None
     dependencies_preprocess_modes: Union[dict, PreprocessMode] = None
     debug: bool = False
-    node_results: Mapping[str, NodeResult] = field(init=False, default=None)
+    node_results: Autodict = field(init=False, default=None)
 
     def copy(self):
         return RunContext(
@@ -70,10 +74,12 @@ class RunContext:
         self.cache_modes = self.cache_modes or {}
 
         self.dependencies_preprocess_modes = self.dependencies_preprocess_modes or {}
-        self.node_results = {
-            node: NodeResult(self.spark, df, schema=None)
-            for node, df in self.provided_inputs.items()
-        }
+
+        self.node_results = Autodict()
+        for node, df in self.provided_inputs.items():
+            self.node_results[node][node] = NodeResult(self.spark, df, schema=None)
+
+    
 
     def update_node_results(
         self,
@@ -86,6 +92,40 @@ class RunContext:
         ],
     ):
         self.node_results[node] = NodeResult(self.spark, df, schema=node.output_schema)
+
+    def populate_cached_successors(self, provided_inputs: dict, execution_graph):
+        """
+        Populate node_results with cached successors of provided input nodes.
+        
+        For each provided input node, finds all successor nodes that are cached
+        and loads their cache into node_results with nested structure:
+        node_results[provided_input_node][cached_successor_node] = NodeResult
+        
+        Parameters
+        ----------
+        provided_inputs : dict
+            Dictionary with Node objects as keys and DataFrames as values
+        execution_graph : NodeGraph
+            The execution graph containing node metadata and relationships
+        """
+        for input_node in provided_inputs.keys():
+            # Get all successors of this input node
+            if input_node.key not in execution_graph.graph:
+                continue
+                
+            for successor_key in execution_graph.graph.successors(input_node.key):
+                # Get the successor node data
+                successor_data = execution_graph.get_node(successor_key)
+                successor_node = successor_data["transformation"]
+                cache_context = successor_data["node_run_context"].cache_context
+                
+                # Check if this successor is cached
+                if cache_context and cache_context.exists_cache_to_load:
+                    # Read from cache and store in nested structure
+                    cached_df = cache_context.read(from_node=input_node, to_node=successor_node)
+                    self.node_results[input_node][successor_node] = NodeResult(
+                        self.spark, cached_df, schema=successor_node.output_schema
+                    )
 
     @property
     def skipped_node_keys(self):
